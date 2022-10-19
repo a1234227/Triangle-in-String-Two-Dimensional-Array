f7b799cadd7aa5aefb833db25f8ad06d60753bda
a1234227 <a1234227@gmail.com>
2022-10-20 00:28:41 +0800

    Update Triangle.java

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,45 +1,48 @@
-import java.util.Scanner;
 
 public class Triangle {
 
 	public static void main(String[] args) {
-		System.out.println("----------程式開始------------");
-		Scanner sc=new Scanner(System.in);
-		System.out.print("請輸入三角形的層數： ");
-		int number =sc.nextInt();
-		System.out.println("");
 		
-		//把指定樣式三角形存入String二維矩陣
-		String[][] result = makeTriangle(number);
-		sc.close();
+		int number =5;
 		
-		//印出來
-		StringBuilder sb = new StringBuilder();
-		for (int i=0; i<result.length; i++) {
-			for (int j=0; j<result[i].length; j++) {
-				sb.append(result[i][j]);
-			}
-		}
-		System.out.println(sb.toString());
+		String[][] Triangle=makeTriangle(number);
+		    
+		    for(int i=0;i<number;i++) {
+		    	for(int q=0;q<2*number+1;q++) {
+		    		System.out.print(Triangle[i][q]);
+		    	}
+		    }
 		
 		
-		System.out.println("----------程式結束------------");
 	}
 
 	public static String[][] makeTriangle(int number) {
-
-		String[][] Triangle = new String[number][number+1];
-
-		for (int i=0; i<number; i++) {
-			for (int j=0; j < number-i-1; j++) {
-				Triangle[i][j]="X";
-			}
-			for (int k=number-i-1; k<number; k++) {
-				Triangle[i][k]="O";
-			}
-
-			Triangle[i][number]="\n";
-		}
+	    
+		String[][] Triangle =new String[number][2*number+1];
+		
+		int x=0;
+		int j=0;
+		    for(int i = 0; i < number ; i++) {       
+		        for(int k =0; k<(number-i-1); k++) {  
+		        	Triangle[i][k]=("X");
+		           }
+		        j=(number-i-1);
+		        for(int y=0 ;y<=x;y++) {
+		        	   Triangle[i][j]=("O");
+		        	   j++;
+		        }
+		        Triangle[i][2*number]=("\n");
+		        x+=2;
+		    }
+		    
+		    for(int a=0;a<number;a++) {
+		    	for(int b=0;b<2*number+1;b++) {
+		    		if(Triangle[a][b]==null) {
+		    			Triangle[a][b]=" ";
+		    		}
+		    	}
+		    }
+	    
 
 		return Triangle;
 
